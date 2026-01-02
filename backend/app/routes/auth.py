b5ddadf525fb220f55016c79ec373141d58656c5
Pedro Tampubolon <bobaimut75@gmail.com>
2026-01-02 12:06:11 +0700

    Update Database, env, and gitignore

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,8 +1,25 @@
 #imports
 from fastapi import APIRouter, Path
 from app.schemas.user import UserLogin, UserRegister, UserUpdate, CashierLogin, CashierRegister, CashierUpdate
+from app.models.user import User
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
+from sqlalchemy import select
 
 router = APIRouter()
+
+class crud():
+    async def get_user(self, async_session: async_sessionmaker[AsyncSession]):
+        async with async_session() as session:
+            statement = select(User).group_by(User.id)
+            result = await session.execute(statement)
+            return result.scalars()
+        
+    async def add_user(self, async_session: async_sessionmaker[AsyncSession]):
+        async with async_session() as session:
+            session.add(user)
+            await session.commit()
+            await session.refresh(user)
+            return user
 
 #admin routes
 #Login route
@@ -20,7 +37,9 @@ def register(user: UserRegister = Path(description="User registration")):
 def update_user(user: UserUpdate = Path(description="User update")):
     return user, {"message": "User updated successfully"}
 
-@router.delete("/delete ")
+@router.delete("/delete/{user_id}")
+def delete_user(user_id: str = Path(description="User ID to delete")):
+    return {"user_id": user_id, "message": "User deleted successfully"}
 
 #cashier routes
 #Login route
@@ -37,3 +56,7 @@ def register_cashiers(user: CashierRegister = Path(description="Cashier registra
 @router.put("/update/cashiers")
 def update_cashiers(user: CashierUpdate = Path(description="Cashier update")):
     return user, {"message": "Cashiers updated successfully"}
+
+@router.delete("/delete/cashiers/{user_id}")
+def delete_cashiers(user_id: str = Path(description="Cashier ID to delete")):
+    return {"user_id": user_id, "message": "Cashiers deleted successfully"}
